fa8f4a03c9849640b13c2fa172e234aab525a222
Steve Simon <mail@pmean.com>
2019-08-22 19:01:28 -0500

    Tweaking data-viz-01

--- common-files/src/setup.R
+++ common-files/src/setup.R
@@ -5,14 +5,15 @@ suppressMessages(suppressWarnings(library(magrittr)))
 suppressMessages(suppressWarnings(library(readr)))
 pd_text <- "This image was produced by Steve Simon and is placed in the public domain. You are welcome to use this image any way you see fit. An acknowledgement would be appreciated, but is not required."
 initiate_image <- function() {
+  if (redraw_graphs==FALSE) return
   fn <- opts_current$get("label")
   png(filename=paste0("../images/r/", fn, ".png"))
-  return(fn)
+  return(invisible(fn))
 }
-finalize_image <- function() {
+finalize_image <- function(caption="") {
+  if (redraw_graphs==FALSE) return
+  fn <- opts_current$get("label")
   quiet <- dev.off()
-  write(pd_text, file=paste0("../images/r/", fn, ".txt"))
+  write(pd_text, file=paste0("../images/r", fn, ".txt"))
+  display_image <<- paste0("![", caption, "](../images/r/", fn, ".png)")
 }
-f <- "https://dasl.datadescription.com/download/data/3275"
-saratoga_houses <- read.table(f, header=TRUE, sep="\x09")
-saratoga_houses$i <- 1:1057
